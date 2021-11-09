5df6882fae0c47fe1e1eddffc69cb7ae14e5dee7
Alisson Ferro <92187207+AlissonFerro@users.noreply.github.com>
2021-11-08 21:04:53 -0300

    Update Buscar.Designer.cs

--- Buscar.Designer.cs
+++ Buscar.Designer.cs
@@ -1,16 +1,16 @@
-﻿namespace WindowsFormsApp1
+namespace WindowsFormsApp1
 {
-    partial class Buscar
+    partial class Disciplina
     {
         /// <summary>
-        /// Variável de designer necessária.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Limpar os recursos que estão sendo usados.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,65 +20,105 @@
             base.Dispose(disposing);
         }
 
-        #region Código gerado pelo Windows Form Designer
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Método necessário para suporte ao Designer - não modifique 
-        /// o conteúdo deste método com o editor de código.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox = new System.Windows.Forms.ListBox();
-            this.btnListar = new System.Windows.Forms.Button();
-            this.btnListarResp = new System.Windows.Forms.Button();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.lblDisciplina = new System.Windows.Forms.Label();
+            this.txtDisciplina = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnVoltar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // listBox
+            // btnCadastrar
             // 
-            this.listBox.FormattingEnabled = true;
-            this.listBox.Location = new System.Drawing.Point(73, 54);
-            this.listBox.Name = "listBox";
-            this.listBox.Size = new System.Drawing.Size(440, 160);
-            this.listBox.TabIndex = 0;
+            this.btnCadastrar.Font = new System.Drawing.Font("Arial Narrow", 12F);
+            this.btnCadastrar.Location = new System.Drawing.Point(32, 161);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(96, 27);
+            this.btnCadastrar.TabIndex = 0;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
             // 
-            // btnListar
+            // lblDisciplina
             // 
-            this.btnListar.Location = new System.Drawing.Point(73, 237);
-            this.btnListar.Name = "btnListar";
-            this.btnListar.Size = new System.Drawing.Size(75, 23);
-            this.btnListar.TabIndex = 1;
-            this.btnListar.Text = "Listar Aluno";
-            this.btnListar.UseVisualStyleBackColor = true;
-            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            this.lblDisciplina.AutoSize = true;
+            this.lblDisciplina.Font = new System.Drawing.Font("Arial Narrow", 12F);
+            this.lblDisciplina.Location = new System.Drawing.Point(28, 28);
+            this.lblDisciplina.Name = "lblDisciplina";
+            this.lblDisciplina.Size = new System.Drawing.Size(125, 20);
+            this.lblDisciplina.TabIndex = 1;
+            this.lblDisciplina.Text = "Nome da Disciplina";
             // 
-            // btnListarResp
+            // txtDisciplina
             // 
-            this.btnListarResp.Location = new System.Drawing.Point(172, 237);
-            this.btnListarResp.Name = "btnListarResp";
-            this.btnListarResp.Size = new System.Drawing.Size(122, 23);
-            this.btnListarResp.TabIndex = 2;
-            this.btnListarResp.Text = "Listar Responsável";
-            this.btnListarResp.UseVisualStyleBackColor = true;
-            this.btnListarResp.Click += new System.EventHandler(this.btnListarResp_Click);
+            this.txtDisciplina.Font = new System.Drawing.Font("Arial Narrow", 12F);
+            this.txtDisciplina.Location = new System.Drawing.Point(32, 51);
+            this.txtDisciplina.Name = "txtDisciplina";
+            this.txtDisciplina.Size = new System.Drawing.Size(121, 26);
+            this.txtDisciplina.TabIndex = 2;
             // 
-            // Buscar
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(241, 54);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(241, 134);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F);
+            this.label1.Location = new System.Drawing.Point(237, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Disciplinas já cadastradas";
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Font = new System.Drawing.Font("Arial Narrow", 12F);
+            this.btnVoltar.Location = new System.Drawing.Point(386, 224);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(96, 27);
+            this.btnVoltar.TabIndex = 5;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // Disciplina
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(638, 321);
-            this.Controls.Add(this.btnListarResp);
-            this.Controls.Add(this.btnListar);
-            this.Controls.Add(this.listBox);
-            this.Name = "Buscar";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(534, 296);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtDisciplina);
+            this.Controls.Add(this.lblDisciplina);
+            this.Controls.Add(this.btnCadastrar);
+            this.Name = "Disciplina";
+            this.Text = "Disciplina";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox;
-        private System.Windows.Forms.Button btnListar;
-        private System.Windows.Forms.Button btnListarResp;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Label lblDisciplina;
+        private System.Windows.Forms.TextBox txtDisciplina;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnVoltar;
     }
 }
