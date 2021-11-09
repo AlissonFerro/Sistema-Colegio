ca8a9031643f929beab0401480aaed6d17cfb181
Alisson Ferro <92187207+AlissonFerro@users.noreply.github.com>
2021-11-08 21:12:35 -0300

    Update Menu.cs

--- Menu.cs
+++ Menu.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
@@ -26,24 +19,28 @@ namespace WindowsFormsApp1
         {
             Cadastro cadastro = new Cadastro();
             cadastro.Show();
+            this.Hide();
         }
 
         private void btnListar_Click(object sender, EventArgs e)
         {
             Buscar buscar = new Buscar();
             buscar.Show();
+            this.Hide();
         }
 
         private void btnDisciplinas_Click(object sender, EventArgs e)
         {
             Disciplina disciplina = new Disciplina();
             disciplina.Show();
+            this.Hide();
         }
 
         private void btnNotas_Click(object sender, EventArgs e)
         {
             Notas notas = new Notas();
             notas.Show();
+            this.Hide();
         }
     }
 }
