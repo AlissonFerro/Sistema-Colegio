d2561e75eaf5c14b06c37494e6574218cb2c379c
Alisson Ferro <92187207+AlissonFerro@users.noreply.github.com>
2021-11-08 21:07:56 -0300

    Update Disciplina.Designer.cs

--- Disciplina.Designer.cs
+++ Disciplina.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApp1
+namespace WindowsFormsApp1
 {
     partial class Disciplina
     {
@@ -33,12 +33,13 @@
             this.txtDisciplina = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnVoltar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCadastrar
             // 
             this.btnCadastrar.Font = new System.Drawing.Font("Arial Narrow", 12F);
-            this.btnCadastrar.Location = new System.Drawing.Point(32, 151);
+            this.btnCadastrar.Location = new System.Drawing.Point(32, 161);
             this.btnCadastrar.Name = "btnCadastrar";
             this.btnCadastrar.Size = new System.Drawing.Size(96, 27);
             this.btnCadastrar.TabIndex = 0;
@@ -50,7 +51,7 @@
             // 
             this.lblDisciplina.AutoSize = true;
             this.lblDisciplina.Font = new System.Drawing.Font("Arial Narrow", 12F);
-            this.lblDisciplina.Location = new System.Drawing.Point(28, 50);
+            this.lblDisciplina.Location = new System.Drawing.Point(28, 28);
             this.lblDisciplina.Name = "lblDisciplina";
             this.lblDisciplina.Size = new System.Drawing.Size(125, 20);
             this.lblDisciplina.TabIndex = 1;
@@ -59,7 +60,7 @@
             // txtDisciplina
             // 
             this.txtDisciplina.Font = new System.Drawing.Font("Arial Narrow", 12F);
-            this.txtDisciplina.Location = new System.Drawing.Point(32, 73);
+            this.txtDisciplina.Location = new System.Drawing.Point(32, 51);
             this.txtDisciplina.Name = "txtDisciplina";
             this.txtDisciplina.Size = new System.Drawing.Size(121, 26);
             this.txtDisciplina.TabIndex = 2;
@@ -67,7 +68,7 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(241, 76);
+            this.listBox1.Location = new System.Drawing.Point(241, 54);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(241, 134);
             this.listBox1.TabIndex = 3;
@@ -76,17 +77,29 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F);
-            this.label1.Location = new System.Drawing.Point(237, 50);
+            this.label1.Location = new System.Drawing.Point(237, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(163, 20);
             this.label1.TabIndex = 4;
             this.label1.Text = "Disciplinas já cadastradas";
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Font = new System.Drawing.Font("Arial Narrow", 12F);
+            this.btnVoltar.Location = new System.Drawing.Point(386, 224);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(96, 27);
+            this.btnVoltar.TabIndex = 5;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
             // 
             // Disciplina
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(534, 296);
+            this.Controls.Add(this.btnVoltar);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.txtDisciplina);
@@ -106,5 +119,6 @@
         private System.Windows.Forms.TextBox txtDisciplina;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnVoltar;
     }
 }
